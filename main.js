49b7f244f377746524c4e8d5b8f7f0ed0d7d9984
oaikhienancornelius97 <corneliusedos@gmail.com>
2022-01-03 18:39:44 +0100

    for a start

---
+++ main.js
@@ -0,0 +1,24 @@
+// function yes(a, b) {
+//     return (a + b);
+// }
+// console.log(yes(2, 4));
+
+// function evenNums(num) {
+//     let evenNumbers = [];
+//     let oddNumbers = [];
+
+//     for (let i = 0; i < num.length; i++){
+//         if (num[i] % 2 === 0) {
+//             evenNumbers.push(num[i])
+//         } else {
+//             oddNumbers.push(num[i])
+//         }
+//     }
+//     return evenNumbers.concat(oddNumbers);
+// }
+
+// console.log(evenNums([1,2,3,4,5,6,7,8,9,10,30,]));
+
+function numWords(params) {
+    
+}
